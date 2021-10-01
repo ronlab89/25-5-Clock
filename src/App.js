1e536b6b8d15e88b737ee47b1774c837f892e9d6
Ronald Labrador <ronaldlabrador89@gmail.com>
2021-10-01 16:50:11 -0400

    Agregados assets faltantes y creado estilo principal de la app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import './assets/css/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import Logo from './assets/images/logo.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header">
+          <img src={Logo} alt="Logo-personal" className="img-fluid" width="70"></img>
+        </div>
+        <section className="watch">
+          <div className="screen"></div>  
+        </section>
+        <div className="footer row">
+          <h1 className="col-6 text-start ps-5">25 + 5 Clock</h1>
+          <p className="col-6 text-end pe-5">Designed and coded by Ronald Labrador - &copy; 2021</p>
+        </div>
       </header>
     </div>
   );
