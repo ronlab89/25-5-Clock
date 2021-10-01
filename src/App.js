f4da3a026ba0d5c929a9782314f81eda6935e8a6
Ronald Labrador <ronaldlabrador89@gmail.com>
2021-10-01 18:50:54 -0400

    Creado componente display

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import './assets/css/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import Logo from './assets/images/logo.png'
+import Logo from './assets/images/logo.png';
+import Display from './components/Display';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,9 +14,7 @@ function App() {
           <img src={Logo} alt="Logo-personal" className="img-fluid" width="70"></img>
         </div>
         <section className="watch">
-          <div className="screen p-2">
-            <h2>reloj</h2>
-          </div>  
+          <Display />
         </section>
         <div className="footer row">
           <h1 className="col-6 text-start ps-5">25 + 5 Clock</h1>
